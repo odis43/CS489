74558d1e65f6644a2a3b6d5e6f095ddbd4a1aa2a
Oditha Amarasinghe <62847596+odis43@users.noreply.github.com>
2025-02-21 13:10:35 -0500

    Update a1q2.c

--- a1q2.c
+++ a1q2.c
@@ -1,16 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
-    
+
 #define BUFSIZE 256
-    
+
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
-    if (argc != 2) { 
+    if (argc != 2) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
-    strcat(cmd, argv[1]);
+
+    char cmd[BUFSIZE] = {0};
+    //quote the argument to be treated as a string
+    snprintf(cmd, BUFSIZE, "wc -c < \"%s\"", argv[1]);
     system(cmd);
 }
