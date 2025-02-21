fd4d3ec7d82fe55437e05c416567abce6bddeae3
Oditha Amarasinghe <62847596+odis43@users.noreply.github.com>
2025-02-20 23:33:34 -0500

    Update a1q2.c

--- a1q2.c
+++ a1q2.c
@@ -3,6 +3,27 @@
 
 #define BUFSIZE 256
 
+
+void encodeShellString(char *dest, size_t size, char *src){
+   size_t j = 0;
+   dest[j] = '"';
+   j++;
+   for (size_t i = 0; src[i] != '\0' && j < size - 2; i++) {
+       if(src[i] == '"'){
+          if(dest[j] < size - 3){
+             dest[j] = '\\';
+          } else {
+              break;
+          }
+       }
+       dest[j] = src[i];
+       j++;
+   }
+   dest[j] = '"';
+   j++;
+   dest[j] = '\0';
+}
+
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
@@ -10,7 +31,8 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char cmd[BUFSIZE] = "wc -c < ";
+    char cmd[BUFSIZE] = {0};
+    encodeShellString(cmd, BUFSIZE, argv[1]);
     strcat(cmd, argv[1]);
     system(cmd);
 }
